20e248ba87826d7633a2495929c52879d633ba73
Nicholas Pompea <nick.pompea@gmail,com>
2023-01-29 15:02:11 -0700

    Initial Commit

---
+++ LockCalc/LockCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockCalcApp.swift
+//  LockCalc
+//
+//  Created by Nicholas Pompea on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct LockCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
